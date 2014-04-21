2c1e5bdc8b6c08023658d444d22c879cf4233604
danferth <danferth@gmail.com>
2014-04-21 09:44:51 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<h1>EulerSolutions</h1>
+
+<p>Using <a href="http://projecteuler.net/">project Euler</a> as a way to make the brain think outside of the normal paradigm.</p>
+<hr>
+<h3>Problems</h3>
+<ol>
+
+<li><a href="/problems/problem-001.php">Multiples of 3 & 5</a></li>
+<li><a href="/problems/problem-002.php">Even Fibonacci Numbers</a></li>
+
+</ol>
---
+++ problems/problem-001.php
@@ -0,0 +1,58 @@
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="/assets/js/prism.js"></script>
+<link rel="stylesheet" href="/assets/css/prism.css">
+<?php
+/*
+*************problem 1****************
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+
+*/
+
+function threeFive(){
+	$base = 0;
+	for ($i = 1; $i < 1000; $i++){
+		if($i%3 === 0 || $i%5 === 0){
+			$base = $base + $i;
+		}
+	}
+	echo $base;
+	
+}
+
+?>
+
+
+
+
+<!-- *********DISPLAY********** -->
+<h2>problem 1</h2>
+<h4>Multiples of 3 and 5</h4>
+<hr>
+
+<p>If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.</p>
+
+<p>Find the sum of all the multiples of 3 or 5 below 1000.</p>
+<hr><!-- *****CODE***** -->
+
+<code class="language-php"><pre class="line-numbers">
+function threeFive(){
+	$base = 0;
+	for ($i = 1; $i < 1000; $i++){
+		if($i%3 === 0 || $i%5 === 0){
+			$base = $base + $i;
+		}
+	}
+	echo $base;
+}
+
+threeFive();
+</pre></code>
+<hr><!-- *****OUTPUT***** -->
+<?php 
+
+threeFive();
+
+ ?>
