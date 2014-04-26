4ff143c569087a0e644cc5119a2705e5b7a15be7
danferth <danferth@gmail.com>
2014-04-25 17:30:34 -0700

    trying 3 again

--- cmd/003.php
+++ cmd/003.php
@@ -9,31 +9,62 @@ What is the largest prime factor of the number 600851475143 ?
 */
 
 function isPrime($test){
-	$sr = ceil(sqrt($test));
-	for ($check=3; $check <= $sr ; $check = $check + 2) { 
+	$limit = ceil($test/2);
+	$count = 0;
+	//is $test less than first jprime number (2)
+	if ($test < 2) {
+		$count++;
+	}
+	//is $test greater than 2 AND even
+	if ($test > 2 && $test % 2 == 0) {
+		$count++;
+	}
+	//at this point we know that $test is NOT a negative number, not 1 and not even, could be two though.
+
+	//now compair $test to all odd numbers between 3 and sqrt or $test
+	for ($check = 3; $check <= $limit; $check = $check + 2) { 
 		if ($test % $check == 0) {
-			return false;
+			$count++;
 		}
 	}
-	return true;
+
+	if ($count > 0) {
+		echo 'NOT prime'.PHP_EOL;
+	}else{
+		echo 'prime'.PHP_EOL;
+	}
 }
 
 
 function prime_factors($test){
 	$start_t = time();
 	$half = ceil($test/2);
+	if ($test % 2 == 0) {
+		echo '2'.PHP_EOL;
+	}
 	//find multiples of $test 
-	for ($check = 2; $check <= $half; $check++) { 
+	for ($check = 3; $check <= $half; $check = $check + 2) { 
 		if ($test % $check == 0) {
-		//	if (isPrime($check)) {
+			if (isPrime($check)) {
 				echo $check.PHP_EOL;
-		//	}
+			}
 		}
 	}
 
 	$end_t = time();
 	$total_t = $end_t - $start_t;
-	echo 'time to complete: '.$total_t.'sec'.PHP_EOL;
+	if ($total_t < 60) {
+	echo 'time to complete: '.$total_t.' sec'.PHP_EOL;
+	}
+	if ($total_t > 61 && $total_t < 3599) {
+		$new_total = $total_t/60;
+		echo 'time to complete: '.$new_total.'min'.PHP_EOL;
+	}
+	if ($total_t > 61 && $total_t > 3599) {
+		$minutes = $total_t/60;
+		$hours = $minutes/60;
+		echo 'time to complete: '.$hours.' hours'.PHP_EOL;
+	}
 
 }
 
--- cmd/test.php
+++ cmd/test.php
@@ -1,20 +1,59 @@
 <?php 
 
 function isPrime($test){
-	$sr = ceil(sqrt($test));
-	for ($check=3; $check <= $sr ; $check = $check + 2) { 
+	$start = time();
+	$limit = ceil($test/2);
+	$count = 0;
+	//is $test less than first jprime number (2)
+	if ($test < 2) {
+		$count++;
+	}
+	//is $test greater than 2 AND even
+	if ($test > 2 && $test % 2 == 0) {
+		$count++;
+	}
+	//at this point we know that $test is NOT a negative number, not 1 and not even, could be two though.
+
+	//now compair $test to all odd numbers between 3 and sqrt or $test
+	for ($check = 3; $check <= $limit; $check = $check + 2) { 
 		if ($test % $check == 0) {
-			return false;
+			echo 'check '.$check.PHP_EOL;
+			$count++;
 		}
 	}
-	return true;
-}
-$x = 331618151217133020331712151816133;
-if (isPrime($x)) {
-	echo $x.' is Prime'.PHP_EOL;
-}else{
-	echo $x.' is NOT Prime'.PHP_EOL;
+
+
+
+
+	if ($count > 0) {
+		echo 'NOT prime'.PHP_EOL;
+	}else{
+		echo 'prime'.PHP_EOL;
+	}
+	echo 'test = '.$test.PHP_EOL;
+	echo 'limit = '.$limit.PHP_EOL;
+	echo 'count = '.$count.PHP_EOL;
+
+
+
+	$end = time();
+	$total = $end - $start;
+	if ($total < 60) {
+	echo 'time to complete: '.$total.' sec'.PHP_EOL;
+	}
+	if ($total > 61 && $total < 3599) {
+		$new_total = $total/60;
+		echo 'time to complete: '.$new_total.'min'.PHP_EOL;
+	}
+	if ($total > 61 && $total > 3599) {
+		$minutes = $total/60;
+		$hours = $minutes/60;
+		echo 'time to complete: '.$hours.' hours'.PHP_EOL;
+	}
 }
 
+isPrime(4289864465);
+
+//4289864465
 
  ?>
