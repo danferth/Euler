101e53046ba516525649c39c037f408ec6f1b359
danferth <danferth@gmail.com>
2014-05-06 15:31:10 -0700

    solved problem 8 on to problem 9

---
+++ cmd/009.php
@@ -0,0 +1,28 @@
+<?php
+include dirname('../').'./assets/class/time.php';
+/*
+*************problem 8****************
+
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+a² + b² = c²
+
+For example, 3² + 4² = 9 + 16 = 25 = 5².
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+
+Find the product abc.
+
+*/
+		
+function problem_9(){
+	$time = new time();
+	
+
+
+	$time->end();
+
+}
+
+problem_9();
+ ?>
--- cmd/test.php
+++ cmd/test.php
@@ -4,7 +4,6 @@ include dirname('../').'./assets/class/time.php';
 function test(){
 $time = new time();
 
-
 $time->end();
 }
 
