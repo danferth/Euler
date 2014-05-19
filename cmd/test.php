1af330a1ac5b1611f6335ddb8a3204a3c8c95aeb
danferth <danferth@gmail.com>
2014-05-19 09:28:40 -0700

    idea of big array and if's to navigate like a 2D grid

--- cmd/test.php
+++ cmd/test.php
@@ -1,13 +1,27 @@
 <?php
 include dirname('../').'./assets/class/time.php';
 
+
+$test = array(
+	array('a','b','c','d','e','f'),
+	array('g','h','i','j','k','l'),
+	array('m','n','o','p','q','r'),
+	array('s','t','u','v','w','x'),
+	array('y','z','A','B','C','D')
+	);
+
+
+echo $test['0']['3'].$test['0']['0'].$test['2']['1'].PHP_EOL;
+
+
+
 function test(){
 $time = new time();
 
 $time->end();
 }
 
-test();
+//test();
 
 
 
